93c53f33cd6e9be204cd23be478de5ee6e4705b5
Abhijeet-Singh14 <102838018+Abhijeet-Singh14@users.noreply.github.com>
2023-11-28 22:13:32 +0530

    Update config.py

--- telegram_upload/config.py
+++ telegram_upload/config.py
@@ -11,8 +11,8 @@ SESSION_FILE = os.path.expanduser('{}/telegram-upload'.format(CONFIG_DIRECTORY))
 def prompt_config(config_file):
     os.makedirs(os.path.dirname(config_file), exist_ok=True)
     click.echo('Go to https://my.telegram.org and create a App in API development tools')
-    api_id = click.prompt('Please Enter api_id', type=int)
-    api_hash = click.prompt('Now enter api_hash')
+    api_id = click.prompt('20762333', type=int)
+    api_hash = click.prompt('97bb560b7e50f706d2ae6e4bd7caed89')
     with open(config_file, 'w') as f:
         json.dump({'api_id': api_id, 'api_hash': api_hash}, f)
     return config_file
